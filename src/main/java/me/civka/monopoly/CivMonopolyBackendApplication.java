f895cc6eb4b966b20f163db0113e3da12b76c018
dima0808 <mamchenko2210@gmail.com>
2025-06-20 00:30:40 +0200

    init commit

---
+++ src/main/java/me/civka/monopoly/CivMonopolyBackendApplication.java
@@ -0,0 +1,13 @@
+package me.civka.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivMonopolyBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CivMonopolyBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/civka/monopoly/CivMonopolyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.civka.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivMonopolyBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
